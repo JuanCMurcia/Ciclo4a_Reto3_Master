20041cdd111b0598f3e6345b51df7022b9f21161
JuanCMurcia <vrinda3d@gmail.com>
2021-12-10 18:50:48 -0500

    Inicio Reto3

---
+++ src/main/java/com/usa/interfaces/UserInterface.java
@@ -0,0 +1,11 @@
+package com.usa.interfaces;
+
+import com.usa.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserInterface extends MongoRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email, String password);
+}
